ee6e0fd20adf4c4611c90fbe28907e5b94ad5854
xuanrin1412 <xuanrin1412@gmail.com>
2024-05-30 17:39:14 +0700

    Implement server active unactive user

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ const io = new Server(server, {
         methods: ["GET", "POST"]
     }
 })
-
+const rooms = {};
 io.on("connection", (socket) => {
     console.log(`User Connected: ${socket.id}`);
     // //Gửi 1 tin đi tất ca
@@ -22,22 +22,37 @@ io.on("connection", (socket) => {
     //  //Nhận mess từ client gửi console.log(data);
     //     socket.broadcast.emit("receive_message",data) // gửi đi tất cả các người dugf dùng chugn web
     //  })
-    socket.on("join_room", (data) => {
-        socket.join(data)
+    socket.on("join_room", (room) => {
+        socket.join(room)
+        if (!rooms[room]) {
+            rooms[room] = []
+        }
+        rooms[room].push(socket.id)
+
+        io.in(room).emit("room_data", {room, users:rooms[room]})
     })
     //gửi đến tất cả ng trong phòng ko bao gồm ng gửi 
-    socket.on("send_message", (data) => {
-        socket.to(data.room).emit("receive_message", data) // gửi tin nhan den room 
-    })
-    // gửi đến tất cả ng trong phòng bao gồm người gửi
     // socket.on("send_message", (data) => {
-    //     const {room,message} = data;
-    //     io.in(room).emit("receive_message",{ message, senderId: socket.id}) // gửi tin nhan den room 
+    //     socket.to(data.room).emit("receive_message", data) // gửi tin nhan den room 
     // })
+    //gửi đến tất cả ng trong phòng bao gồm người gửi
+    socket.on("send_message", (data) => {
+        const { room, message } = data;
+        const timestamp = new Date();
+        io.in(room).emit("receive_message", { message, senderId: socket.id, timestamp }) // gửi tin nhan den room 
+    })
 
     socket.on('disconnect', () => {
         console.log(`User Disconnect: ${socket.id}`);
-      });
+        for (let room in rooms) {
+            if(rooms[room].includes(socket.id)){
+                rooms[room] = rooms[room].filter(id => id !== socket.id);
+                io.in(room).emit("room_data", { room, users: rooms[room] });
+                io.in(room).emit("user_left", { userId: socket.id});
+            }
+           
+        }
+    });
 });
 
 server.listen(3001, () => {
